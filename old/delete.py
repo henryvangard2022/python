6a35a82e4b875e6de4022fb01fc500fc5eb00e14
henryvangard2022 <110300626+henryvangard2022@users.noreply.github.com>
2022-09-30 12:26:01 -0500

    Delete delete.py

--- old/delete.py
+++
@@ -1,9 +0,0 @@
-# this is the customer module which manipulates the 'customer' table of the  MySQL database 'customercrm'
-import customer as CUS
-
-
-#####################################################
-# DELETE SQL command implemented as separate module #
-#####################################################
-
-delSQL = 'DELETE FROM customer WHERE customer_id = %s'
